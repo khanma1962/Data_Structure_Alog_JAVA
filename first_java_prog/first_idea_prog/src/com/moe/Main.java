5bc8e42d8b4e850f6d782902e88055b465ebe307
Moe Khan <m_a_khan_usa@yahoo.com>
2022-02-05 22:17:57 -0800

    Codes run

---
+++ first_java_prog/first_idea_prog/src/com/moe/Main.java
@@ -0,0 +1,13 @@
+package com.moe;
+
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Please input some number : ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
